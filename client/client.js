c839a231eeca89dd10f00684b44d533af16b82b9
Kevin <kevidently@gmail.com>
2018-02-10 09:43:24 -0700

    Improving compatibility with GPX files. Now using DOMParser and XPath to get data.

--- client/client.js
+++ client/client.js
@@ -1,102 +1,177 @@
 //TEMPLATE ON-RENDERED FUNCTIONALITY
 Template.viewTrack.onRendered(function () {
-    this.autorun(function () {
-        var trackData = Template.currentData();
-        outputCharts(trackData, ["ElevVsTime", "ElevVsDist"], 4000);
-    });
-    //hard-coding this value, route name is not gallery but need to use something
-    updateNav('gallery');
+	this.autorun(function () {
+		var trackData = Template.currentData();
+		outputCharts(trackData, ["ElevVsTime", "ElevVsDist"], 4000);
+	});
+	//hard-coding this value, route name is not gallery but need to use something
+	updateNav('gallery');
 });
 
 //using several of these template onrendered's to update navbar links
 Template.gallery.onRendered(function () {
-    updateNav(Router.current().route.getName());
+	updateNav(Router.current().route.getName());
 });
 
 Template.upload.onRendered(function () {
-    updateNav(Router.current().route.getName());
+	updateNav(Router.current().route.getName());
 });
 
 Template.about.onRendered(function () {
-    this.autorun(function () {
-        var trackData = Template.currentData();
-        outputCharts(trackData, ["ElevVsDist", ], 3000);
-    });
-    updateNav(Router.current().route.getName());
+	this.autorun(function () {
+		var trackData = Template.currentData();
+		outputCharts(trackData, ["ElevVsDist", ], 3000);
+	});
+	updateNav(Router.current().route.getName());
 });
 
 Template.main.onRendered(function () {
-    //add random background image
+	//add random background image
 
-    Meteor.call('randBGImg', function (err, result) {
-        if (err) {
-            console.log(err);
-        }
+	Meteor.call('randBGImg', function (err, result) {
+		if (err) {
+			console.log(err);
+		}
 
-        //use img onload to animate
-        //issue with getting width of img not being ready
-        var bgimg = $('#bgImage')[0];
-        bgimg.onload = function () {
-            //triggers animaion of background
-            var moveAmt = window.innerWidth - this.width;
-            this.style.transform = 'translateX(' + moveAmt + 'px)';
-        };
-        //note, path needs to start with slash, bug in iron router?
-        bgimg.src = '/bgImgs/' + result;
-    });
+		//use img onload to animate
+		//issue with getting width of img not being ready
+		var bgimg = $('#bgImage')[0];
+		bgimg.onload = function () {
+			//triggers animaion of background
+			var moveAmt = window.innerWidth - this.width;
+			this.style.transform = 'translateX(' + moveAmt + 'px)';
+		};
+		//note, path needs to start with slash, bug in iron router?
+		bgimg.src = '/bgImgs/' + result;
+	});
 });
-
-
 
 //TEMPLATE HELPERS
 Template.gallery.helpers({
-    tracks: function () {
-        return Tracks.find().fetch();
-    }
+	tracks: function () {
+		return Tracks.find().fetch();
+	}
 });
-
-
 
 //TEMPLATE EVENTS
 Template.upload.events({
-    'mousedown #uploadBtnHolder': function (event) {
-        $('#uploadBtnHolder').css('transform', 'scale(.9,.9)');
-        $('#uploadBtnHolder').css('text-shadow', '0px 0px 0px black');
-        setTimeout(function () {
-            $('#uploadBtnHolder').css('transform', 'scale(1,1)');
-            $('#uploadBtnHolder').css('text-shadow', '3px 3px 2px black');
-        }, 40);
-    },
-    'mouseup #uploadBtnHolder': function (event) {
-        $('#fileUpload').trigger('click');
-    },
-    'change #fileUpload': function (event) {
-        $('#uploadBtnHolder').css('display', 'none');
-        $('#uploadContent').css('display', 'none');
-        var fileInfo = event.currentTarget.files[0];
-        var reader = new FileReader();
+	'mousedown #uploadBtnHolder': function (event) {
+		$('#uploadBtnHolder').css('transform', 'scale(.9,.9)');
+		$('#uploadBtnHolder').css('text-shadow', '0px 0px 0px black');
+		setTimeout(function () {
+			$('#uploadBtnHolder').css('transform', 'scale(1,1)');
+			$('#uploadBtnHolder').css('text-shadow', '3px 3px 2px black');
+		}, 40);
+	},
+	'mouseup #uploadBtnHolder': function (event) {
+		$('#fileUpload').trigger('click');
+	},
+	'change #fileUpload': function (event) {
+		$('#uploadBtnHolder').css('display', 'none');
+		$('#uploadContent').css('display', 'none');
+		var fileInfo = event.currentTarget.files[0];
+		var reader = new FileReader();
 
-        reader.onload = function (event) {
-            var fileContent = reader.result;
+		reader.onload = function (event) {
+			var fileContent = reader.result;
 
-            Meteor.call('fileUpload', fileContent, function (err, result) {
-                //if this content already in mongo, send the data to makeChart
-                if (result.exists == true) {
-                    Router.go('/viewTrack/' + result.hash);
-                } else {
-                    //if not in mongo, needs to be parsed
-                    var hash = result.hash;
-                    var xmlData = result.xmlData;
-                    Meteor.call('parseAndOutput', xmlData, hash, function (err, result) {
-                        Router.go('/viewTrack/' + result._id);
-                    });
-                }
-            });
-        };
-        //this triggers the reader.onload step above
-        //we don't really do anything with it
-        reader.readAsText(fileInfo);
-    }
+			Meteor.call('fileUpload', fileContent, function (err, result) {
+				// If this content already in mongo, send the data to makeChart
+				if (result.exists == true) {
+					Router.go('/viewTrack/' + result.hash);
+				} else {
+					// If not in mongo, needs to be parsed and then analyzed
+					
+					// Although it would be good to do the majority of this on the server,
+					// it seems NodeJS does not offer 'DOMParser', etc
+					
+					var hash = result.hash;
+					var fileData = result.fileData;
+					
+					// Create an xml doc from filedata
+					var dp = new DOMParser();
+					var xmlDoc = dp.parseFromString(fileData, "application/xml");
+					
+					// Setup object to store data for this track
+					var trackData = {
+						_id: hash, // becomes mongo id
+						name: '',
+						desc: '',
+						locations: [],
+						totalDist: 0,
+						totalTime: 0,
+						elevMax: -Infinity,
+						elevMin: Infinity,
+						elevMaxIndex: 0
+					};
+
+					var nsResolver = function (prefix) {
+						return prefix === "g" ? "http://www.topografix.com/GPX/1/1" : null;
+					}
+
+					// Start xpath queries ('g:' is custom prefix for default namespace)
+
+					// Get <name>
+					var nameResult = document.evaluate("/g:gpx/g:trk/g:name", xmlDoc, nsResolver, XPathResult.STRING_TYPE, null);
+					trackData.name = nameResult.stringValue;
+
+					// Get <desc>
+					var descResult = document.evaluate("/g:gpx/g:trk/g:desc", xmlDoc, nsResolver, XPathResult.STRING_TYPE, null);
+					trackData.desc = descResult.stringValue;
+
+					var trkPtResult = document.evaluate("//g:trkpt", xmlDoc, nsResolver, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+
+					// Iterate all '<trkpt>' and gather info
+					var trkPtNode;
+					while (trkPtNode = trkPtResult.iterateNext()) {
+
+						// Store info for this location
+						var locObj = {};
+
+						// Get lat and long
+						var trkPtAttrs = trkPtNode.attributes;
+						var latAttr = trkPtAttrs.getNamedItem("lat");
+						var lonAttr = trkPtAttrs.getNamedItem("lon");
+
+						// Get elevation and time
+						var childNodes = trkPtNode.children;
+						var elevation, timestamp;
+
+						for ( var i in childNodes ) {
+							var childNode = childNodes[i];
+							if ( childNode.nodeName == "ele" ) {
+								elevation = childNode.textContent;
+							}
+							if ( childNode.nodeName == "time" ) {
+								timestamp = childNode.textContent;
+							}
+						}
+
+						// Setting lat and long
+						locObj.lat = latAttr.value;
+						locObj.lon = lonAttr.value;
+
+						// Setting elevation
+						locObj.elev = elevation * 3.281; // conversion meters to feet
+
+						// Setting timestamp
+						// ISO 8601 formatted string: 2015-05-25T15:58:18.608Z (motionX)
+						// variant seen in GaiaGPS: 2018-02-04T17:37:19Z
+						var tmpDate = new Date(timestamp);
+						locObj.timestamp = tmpDate.getTime();
+
+						trackData.locations.push(locObj);
+					}
+
+					Meteor.call('analyzeAndOutput', trackData, function (err, result) {
+						Router.go('/viewTrack/' + result._id);
+					});
+				}
+			});
+		};
+		//this triggers the reader.onload step above
+		reader.readAsText(fileInfo);
+	}
 });
 
 
@@ -105,42 +180,42 @@ Template.upload.events({
 
 //re-animate bg image if window resize
 window.onresize = function () {
-    var bgimg = $('#bgImage')[0];
-    var moveAmt = window.innerWidth - bgimg.width;
+	var bgimg = $('#bgImage')[0];
+	var moveAmt = window.innerWidth - bgimg.width;
 
-    //halt the transition
-    bgimg.className = 'notransition';
+	//halt the transition
+	bgimg.className = 'notransition';
 
-    //re-position image
-    bgimg.style.top = 0;
-    bgimg.style.left = 0;
-    bgimg.style.height = '100vh';
-    bgimg.style.transform = 'translateX(0px)';
+	//re-position image
+	bgimg.style.top = 0;
+	bgimg.style.left = 0;
+	bgimg.style.height = '100vh';
+	bgimg.style.transform = 'translateX(0px)';
 
-    //need to trigger a 'reflow' of css changes
-    bgimg.offsetHeight; //'trigger a css reflow, flush css changes'
+	//need to trigger a 'reflow' of css changes
+	bgimg.offsetHeight; //'trigger a css reflow, flush css changes'
 
-    //allow new transition to start
-    bgimg.removeAttribute('class');
+	//allow new transition to start
+	bgimg.removeAttribute('class');
 
-    //trigger animation
-    bgimg.style.transform = 'translateX(' + moveAmt + 'px)';
+	//trigger animation
+	bgimg.style.transform = 'translateX(' + moveAmt + 'px)';
 };
 
 
 //Update borders on nav links
 function updateNav(routeName) {
-    $('.navLink').css('border', '0px');
-    $('#nav_' + routeName).css('border-top', '2px solid #fbc901');
-    $('#nav_' + routeName).css('border-bottom', '2px solid #fbc901');
+	$('.navLink').css('border', '0px');
+	$('#nav_' + routeName).css('border-top', '2px solid #fbc901');
+	$('#nav_' + routeName).css('border-bottom', '2px solid #fbc901');
 }
 
 //Wrapper for "Make Chart"
 function outputCharts(trackObj, types, duration) {
-    // remove last loc, doesn't have "current distVal"
-    trackObj.locations.pop();
-    document.getElementById("charts").innerHTML = "";
-    for (var i in types) {
-        duration ? makeChart(trackObj, types[i], duration) : makeChart(trackObj, types[i])
-    }
+	// remove last loc, doesn't have "current distVal"
+	trackObj.locations.pop();
+	document.getElementById("charts").innerHTML = "";
+	for (var i in types) {
+		duration ? makeChart(trackObj, types[i], duration) : makeChart(trackObj, types[i])
+	}
 }
--- server/server.js
+++ server/server.js
@@ -1,216 +1,144 @@
 Meteor.startup(function () {
-//    Tracks.remove({});
-    if (Tracks.find().count() === 0) {
-        console.log("Tracks collection is empty");
-    }
+	Tracks.remove({});
+	if (Tracks.find().count() === 0) {
+		console.log("Tracks collection is empty");
+	}
 });
 
 //needed for routing
 Meteor.publish('tracks', function () {
-    return Tracks.find();
+	return Tracks.find();
 });
 
 Meteor.methods({
-    randBGImg: function () {
-        //generate a random bg image
-        var fs = Npm.require('fs');
-        var meteorRoot = fs.realpathSync( process.cwd() + '/../' );
-        var publicPath = meteorRoot + '/web.browser/app/';   
-        var dirFiles = fs.readdirSync(publicPath +'bgImgs/');
-        var rand = Math.floor(Math.random()*dirFiles.length);
-        return dirFiles[rand];
-    },
-    fileUpload: function (fileData) {
-        
-        //create md5 checksum of file
-        var crypto = Npm.require('crypto');
+	randBGImg: function () {
+		//generate a random bg image
+		var fs = Npm.require('fs');
+		var meteorRoot = fs.realpathSync( process.cwd() + '/../' );
+		var publicPath = meteorRoot + '/web.browser/app/';   
+		var dirFiles = fs.readdirSync(publicPath +'bgImgs/');
+		var rand = Math.floor(Math.random()*dirFiles.length);
+		return dirFiles[rand];
+	},
+	fileUpload: function (fileData) {
+		
+		//create md5 checksum of file
+		var crypto = Npm.require('crypto');
 
-        function checksum(str, algorithm, encoding) {
-            return crypto
-                .createHash(algorithm || 'md5')
-                .update(str, 'utf8')
-                .digest(encoding || 'hex')
-        }
+		function checksum(str, algorithm, encoding) {
+			return crypto
+				.createHash(algorithm || 'md5')
+				.update(str, 'utf8')
+				.digest(encoding || 'hex')
+		}
 
-        var hashedFile = checksum(fileData);
+		var hashedFile = checksum(fileData);
 
-        //check if this track has already been processed
-        var trackCheck = Tracks.findOne({
-            _id: hashedFile
-        });
-        if (trackCheck) {
-            //track exists in mongo
-            return {
-                exists: true,
-                hash: hashedFile,
-                jsonData: trackCheck
-            }
-        } else {
-            //track does not exist in mongo
-            return {
-                exists: false,
-                hash: hashedFile,
-                xmlData: fileData
-            }
-        }
-    },
-    storeImgData: function (imgTag, trackId) {
-        var trackData = Tracks.findOne({
-                _id: trackId
-            })
-        if (!trackData.imgTag) {
-            Tracks.update(
-               { _id: trackId }, {$set: {imgTag: imgTag}}
-            );
-        }
-    },
-    parseAndOutput: function (fileData, hash) {
+		//check if this track has already been processed
+		var trackCheck = Tracks.findOne({
+			_id: hashedFile
+		});
+		if (trackCheck) {
+			//track exists in mongo
+			return {
+				exists: true,
+				hash: hashedFile,
+				jsonData: trackCheck
+			}
+		} else {
+			//track does not exist in mongo
+			return {
+				exists: false,
+				hash: hashedFile,
+				fileData: fileData
+			}
+		}
+	},
+	storeImgData: function (imgTag, trackId) {
+		var trackData = Tracks.findOne({
+				_id: trackId
+			})
+		if (!trackData.imgTag) {
+			Tracks.update(
+			   { _id: trackId }, {$set: {imgTag: imgTag}}
+			);
+		}
+	},
+	analyzeAndOutput: function (trackData) {
 
-        //split file data on new lines
-        var fileArr = fileData.split("\n");
+		//go through array of location objects
+		//calculate deltas for elevation, time, and dist between locations
+		for (var i = 0; i < trackData.locations.length; i++) {
 
-        //setup obj to store track data
-        var trackData = {
-            _id: hash, // becomes mongo id
-            name: '',
-            desc: '',
-            locations: [],
-            totalDist: 0,
-            totalTime: 0,
-            elevMax: -Infinity,
-            elevMin: Infinity,
-            elevMaxIndex: 0
-        };
+			//need the "i+1" index in the array
+			var j = i + 1;
 
-        //go through file array content
-        //manipulate data and store in object
-        for (var i in fileArr) {
+			if (trackData.locations[j]) { // if there's a "next" location
 
-            //address one "line" in the file
-            var line = fileArr[i];
+				//distance deltas
+				//setup coordinates
+				var deltaDist = distBtwnPoints(
+					trackData.locations[i].lat,
+					trackData.locations[i].lon,
+					trackData.locations[j].lat,
+					trackData.locations[j].lon
+				);
 
-            //check for name of track
-            var nameCheck = line.match(/^<name>(.*)<\/name>$/);
-            if (nameCheck) {
-                trackData['name'] = nameCheck[1];
-            }
+				//UNIT CONVERSION
+				deltaDist = deltaDist * 0.621; //Km to miles
 
-            //check for desc of track
-            var descCheck = line.match(/^<desc>(.*)<\/desc>$/);
-            if (descCheck) {
-                trackData['desc'] = descCheck[1];
-            }
+				trackData.locations[i].currentDistVal = trackData.totalDist;
+				trackData.totalDist += deltaDist;
+				trackData.locations[i].nextDistVal = trackData.totalDist;
 
-            //check if we are starting new "trkpt"
-            //formatted like: <trkpt lat="39.9579604" lon="-105.3386374">
-            var trkPtCheck = line.match(/^<trkpt lat="(.*)" lon="(.*)">$/);
-            if (trkPtCheck) {
-                var locObj = {
-                    lat: trkPtCheck[1],
-                    lon: trkPtCheck[2],
-                }
-            }
+				//get elev delta with next location -- need for graphing
+				trackData.locations[i].nextElev = trackData.locations[j].elev;
 
-            //check for elevation
-            var eleCheck = line.match(/^<ele>(.*)<\/ele>$/);
-            if (eleCheck && locObj) {
-                //UNIT CONVERSION
-                locObj['elev'] = eleCheck[1] * 3.281; //in feet
-            }
+				//get time delta with next location -- need for graphing  
+				var deltaTime = trackData.locations[j].timestamp - trackData.locations[i].timestamp;
 
-            //check for timestamp
-            //the ts is ISO formatted: 2015-05-25T15:58:18.608Z
-            var tsCheck = line.match(/^<time>(\d+)-(\d+)-(\d+)T(\d+):(\d+):(\d+)\.(\d+)Z<\/time>$/);
-            if (tsCheck && locObj) {
-                // 1=year, 2=month, 3=day, 4=hour, 5=min, 6=sec, 7=milsec  
-                var dateObj = new Date(tsCheck[1], tsCheck[2] - 1, tsCheck[3], tsCheck[4], tsCheck[5], tsCheck[6], tsCheck[7]);
-                locObj['timestamp'] = dateObj.getTime();
-            }
+				//UNIT CONVERSION
+				deltaTime = ((deltaTime / 60000) / 60); //Milsec to hrs
 
+				trackData.locations[i].timeProgress = trackData.totalTime
+				trackData.totalTime += deltaTime;
+			}
 
-            //check if we are done with "trkpt"
-            if (line.match(/^<\/trkpt>$/)) {
-                //push location data to array of locations
-                trackData['locations'].push(locObj);
-            }
-        }
+			//find max elev -- for axis domain
+			if (trackData.locations[i].elev > trackData.elevMax) {
+				trackData.elevMax = trackData.locations[i].elev;
+				trackData.elevMaxIndex = i;
+			}
 
-        //go through array of location objects
-        //calc deltas for elevation, time, and dist between locations
-        for (var i = 0; i < trackData.locations.length; i++) {
+			//find min elev -- for axis domain
+			if (trackData.locations[i].elev < trackData.elevMin) {
+				trackData.elevMin = trackData.locations[i].elev;
+			}
 
-            //need the "i+1" index in the array
-            var j = i + 1;
+		}
 
-            if (trackData.locations[j]) { // if there's a "next" location
+		//function to calculate distance between two points
+		//found here:  http://jsperf.com/haversine-salvador/27
+		function distBtwnPoints(lat1, lon1, lat2, lon2) {
+			var deg2rad = 0.017453292519943295; // === Math.PI / 180
+			var cos = Math.cos;
+			lat1 *= deg2rad;
+			lon1 *= deg2rad;
+			lat2 *= deg2rad;
+			lon2 *= deg2rad;
+			var a = (
+				(1 - cos(lat2 - lat1)) +
+				(1 - cos(lon2 - lon1)) * cos(lat1) * cos(lat2)
+			) / 2;
 
-                //distance deltas
-                //setup coordinates
-                var deltaDist = distBtwnPoints(
-                    trackData.locations[i].lat,
-                    trackData.locations[i].lon,
-                    trackData.locations[j].lat,
-                    trackData.locations[j].lon
-                );
+			return 12742 * Math.asin(Math.sqrt(a)); // Diameter of the earth in km (2 * 6371)
+		}
 
-                //UNIT CONVERSION
-                deltaDist = deltaDist * 0.621; //Km to miles
-
-                trackData.locations[i].currentDistVal = trackData.totalDist;
-                trackData.totalDist += deltaDist;
-                trackData.locations[i].nextDistVal = trackData.totalDist;
-
-                //get elev delta with next location -- need for graphing
-                trackData.locations[i].nextElev = trackData.locations[j].elev;
-
-                //get time delta with next location -- need for graphing  
-                var deltaTime = trackData.locations[j].timestamp - trackData.locations[i].timestamp;
-
-                //UNIT CONVERSION
-                deltaTime = ((deltaTime / 60000) / 60); //Milsec to hrs
-
-                trackData.locations[i].timeProgress = trackData.totalTime
-                trackData.totalTime += deltaTime;
-            }
-
-            //find max elev -- for axis domain
-            if (trackData.locations[i].elev > trackData.elevMax) {
-                trackData.elevMax = trackData.locations[i].elev;
-                trackData.elevMaxIndex = i;
-            }
-
-            //find min elev -- for axis domain
-            if (trackData.locations[i].elev < trackData.elevMin) {
-                trackData.elevMin = trackData.locations[i].elev;
-            }
-
-        }
-
-        //function to calculate distance between two points
-        //found here:  http://jsperf.com/haversine-salvador/27
-        function distBtwnPoints(lat1, lon1, lat2, lon2) {
-            var deg2rad = 0.017453292519943295; // === Math.PI / 180
-            var cos = Math.cos;
-            lat1 *= deg2rad;
-            lon1 *= deg2rad;
-            lat2 *= deg2rad;
-            lon2 *= deg2rad;
-            var a = (
-                (1 - cos(lat2 - lat1)) +
-                (1 - cos(lon2 - lon1)) * cos(lat1) * cos(lat2)
-            ) / 2;
-
-            return 12742 * Math.asin(Math.sqrt(a)); // Diameter of the earth in km (2 * 6371)
-        }
-
-        //store data in mongo and return data to client
-        if (Tracks.findOne({
-                _id: hash
-            })) {
-            return trackData;
-        } else {
-            Tracks.insert(trackData);
-            return trackData;
-        }
-    }
+		// Store data in mongo and return data to client
+		if ( !Tracks.findOne( { _id: trackData._id } ) ) {
+			Tracks.insert(trackData);
+		}
+		
+		return trackData;
+	}
 });
